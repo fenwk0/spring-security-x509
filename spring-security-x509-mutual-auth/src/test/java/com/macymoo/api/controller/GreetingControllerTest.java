9252300d1aaa16d91e6ee68cafdb808df3534ab3
fenwk0 <karl.fenwick@gmail.com>
2019-04-06 12:07:14 +0100

    Added tests

---
+++ spring-security-x509-mutual-auth/src/test/java/com/macymoo/api/controller/GreetingControllerTest.java
@@ -0,0 +1,16 @@
+package com.macymoo.api.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GreetingControllerTest {
+    @Test
+    public void contextLoads() {
+    }
+
+
+}
